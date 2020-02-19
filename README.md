123d1ec4094c20a4a4510bebecd7ea1666ac196e
Nga Chat <mrngachat@gmail.com>
2020-02-19 13:49:56 +0630

    .

---
+++ README.md
@@ -0,0 +1,49 @@
+# react-native-super-background-service 
+
+
+
+
+## Requirements
+
+-   RN 0.36+
+-   Android API 16+
+
+## Supported platforms
+
+-   Android
+
+## Getting started
+
+`$ yarn add react-native-super-background-service`
+
+or
+
+`$ npm install react-native-super-background-service --save`
+
+
+#### Examples
+
+
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import Background from 'react-native-super-background-service'
+
+
+export default class App extends Component {
+ 
+
+
+
+  
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity onPress={()=>Background.startService()} >
+          <Text>Hello, world!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
---
+++ android/app/src/main/java/com/rr/BackgroundService.java
@@ -0,0 +1,83 @@
+package com.rr;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.os.Build;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import com.facebook.react.HeadlessJsTaskService;
+
+public class BackgroundService extends Service {
+
+    private static final int SERVICE_NOTIFICATION_ID = 12345;
+    private static final String CHANNEL_ID = "BACKGROUND";
+
+    private Handler handler = new Handler();
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            Context context = getApplicationContext();
+            Intent myIntent = new Intent(context, BackgroundEventService.class);
+            context.startService(myIntent);
+            HeadlessJsTaskService.acquireWakeLockNow(context);
+            handler.postDelayed(this, 2000);
+        }
+    };
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "BACKGROUND", importance);
+            channel.setDescription("CHANEL DESCRIPTION");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.handler.removeCallbacks(this.runnableCode);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        this.handler.post(this.runnableCode);
+        createNotificationChannel();
+       
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Background service")
+                .setContentText("Running...")
+                .setSmallIcon(R.mipmap.ic_launcher)
+               
+                .setOngoing(true)
+                .build();
+        startForeground(SERVICE_NOTIFICATION_ID, notification);
+        return START_STICKY;
+    }
+
+}
